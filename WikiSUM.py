ab484dbd7473028cc7e54587761364cac325edfb
MiraiX <117575492+666hwll@users.noreply.github.com>
2024-06-16 13:00:30 +0200

    Update WikiSUM.py
    added menubar

--- WikiSUM.py
+++ WikiSUM.py
@@ -3,16 +3,20 @@ import gtts
 import transformers
 import tkinter 
 from tkinter import messagebox
+from tkinter import filedialog
 import gc
 from just_playback import Playback 
-
+from tkinter import Menu
 
 class programm:
 
     def __init__(self):
+        self.pipe = ""
         self.results: str = ""
         self.readable: str = ""
+        self.results_sum: dict = {}
         self.state: bool = False
+        self.pipe_state: bool = False
         self.question_answerer = transformers.pipeline("question-answering", model="Falconsai/question_answering_v2")
         self.summarizer = transformers.pipeline("summarization", model="Falconsai/text_summarization")
         self.tts = Playback()
@@ -24,6 +28,20 @@ class programm:
 
     def _close(self, event) -> None:
         self.onclose()
+
+    def open_file(self) -> None:
+        p: str = filedialog.askopenfilename()
+        with open(p, encoding="utf-8") as f:
+            query = f.read()
+            self.pipe = str(query)
+            self.pipe_state = True
+        
+
+    def save_file(self) -> None:
+        text = self.results + '\n\n' + str(self.results_sum[0]['summary_text']) + '\n\n' + self.readable + '\n\n'
+        p:str = filedialog.asksaveasfile(mode='w')
+        p.write(text)
+
 
     def change(self, userin: str, dl:bool, iNs:bool) -> None:
         if dl:
@@ -38,38 +56,56 @@ class programm:
         gc.collect(0)
 
 
+
     def get_sum(self) -> None:
-        query = entrypoint.get()
-        if query == "":
-            self.change("No text provided", True, True)
-        else:
-            self.change("" ,True, False)
-            entrypoint.delete(0, tkinter.END)
+        query = ""
+        if self.pipe_state == True:
+            query = self.pipe
+                
+        else: 
+            query = str(entrypoint.get())
+            if query == "":
+                self.change("No text provided", True, True)
+        self.pipe_state = False
+        
             
-            gc.collect(1)
-            self.results = wikipedia.summary(query, auto_suggest=True)
-            results_sum = self.summarizer(self.results, max_length=1000, do_sample=False)
-            self.readable = str(results_sum[0]['summary_text'])
-            self.state = False
-            self.change(self.readable, False, True)
+        self.change("" ,True, False)
+        entrypoint.delete(0, tkinter.END)
+            
+        gc.collect(1)
+            
+        self.results = wikipedia.summary(query, auto_suggest=True)
+            
+        self.results_sum = self.summarizer(self.results, max_length=1000, do_sample=False)
+        self.readable = str(self.results_sum[0]['summary_text'])
+        self.state = False
+        self.change(self.readable, False, True)
+
     def _sum(self, event) -> None:
         self.get_sum()
 
     def answer_Q(self) -> None:
-        query = str(entrypoint.get())
-        if query != "":
-            if self.results == "":
-                self.change("No context provided", True, True)
-            else:
-                entrypoint.delete(0, tkinter.END)
+        query = ""
+        if self.pipe_state == True: 
+            query = self.pipe
+            self.pipe_state = False
+            
+        else:
+            query = entrypoint.get()
+            if query != "":
+                if self.results == "":
+                    self.change("No context provided", True, True)
+                else:
+                    entrypoint.delete(0, tkinter.END)
+
                 
                 gc.collect(0)
                 a = self.question_answerer(question=query, context=self.results)
                 self.readable = a['answer']
                 self.state = False
                 self.change(self.readable, True, True)
-        else:
-            self.change("No question provided", True, True)
+            else:
+                self.change("No question provided", True, True)
     
 
     def speak_r(self) -> None:
@@ -81,9 +117,9 @@ class programm:
         else:
             text = "Did not found valid text"
         obj = gtts.gTTS(text=text, lang='en', slow=False)
-        a: str = 'out.mp3'
-        obj.save(a)
-        self.tts.load_file(a)
+        f: str = 'out.mp3'
+        obj.save(f)
+        self.tts.load_file(f)
         self.tts.play()
 
 
@@ -98,6 +134,19 @@ def main() -> None:
     window.title("WikiSUM")
     window.geometry("800x800")
     window.resizable(1,1)
+
+    menubar = Menu(window)
+    window.config(menu=menubar)
+
+    file_menu = Menu(menubar)
+    file_menu.add_command(label='Open', command=processs.open_file)
+    file_menu.add_command(label='Save as file', command=processs.save_file)
+    file_menu.add_command(label='Exit', command=window.destroy)
+    menubar.add_cascade(label='File', menu=file_menu)
+
+    about_menu = Menu(menubar)
+    menubar.add_cascade(label='About', menu=about_menu)
+
     window.bind("<Control-q>", processs._close)
     global entrypoint
     entrypoint = tkinter.Entry(window, width=19)
